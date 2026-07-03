0d70990e6b6df106e6987aeb83ed82e6cd15ecce
William <w.slooten7@upcmail.nl>
2026-07-03 16:28:15 +0200

    Lesson 5 - Create AWS VPC with Terraform

---
+++ 02-vpc/terraform.tfvars
@@ -0,0 +1,3 @@
+aws_region   = "eu-west-1"
+project_name = "eks-bootcamp"
+vpc_cidr     = "10.0.0.0/16"
---
+++ 02-vpc/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS region where resources will be created"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
