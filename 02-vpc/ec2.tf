d77a3095c3d4a9a35fb57adf2ba21e30a8eae859
William <w.slooten7@upcmail.nl>
2026-07-13 14:01:49 +0200

    Lesson 11 - Add EC2 instance and Docker setup

---
+++ 02-vpc/ec2.tf
@@ -0,0 +1,40 @@
+resource "aws_key_pair" "bootcamp" {
+  key_name   = "${var.project_name}-key"
+  public_key = file("${path.module}/eks-bootcamp-key.pub")
+
+  tags = {
+    Name    = "${var.project_name}-key"
+    Project = var.project_name
+  }
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "bootcamp" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.public_1.id
+  vpc_security_group_ids = [aws_security_group.ec2.id]
+  key_name               = aws_key_pair.bootcamp.key_name
+
+  associate_public_ip_address = true
+
+  tags = {
+    Name    = "${var.project_name}-ec2"
+    Project = var.project_name
+  }
+}
---
+++ 02-vpc/security-groups.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "ec2" {
+  name        = "${var.project_name}-ec2-sg"
+  description = "Security Group for EC2 Bootcamp"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+
+    from_port = 0
+    to_port   = 0
+
+    protocol = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "${var.project_name}-ec2-sg"
+    Project = var.project_name
+  }
+}
